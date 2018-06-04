e2760eac2fa88429026dd82a7cedca3d26ae9463
Joshua <jyoung@student.neumont.edu>
2018-06-04 10:12:06 -0600

    Pages are Linked

--- app/src/main/java/com/example/blake/groupfinder/MainActivity.java
+++ app/src/main/java/com/example/blake/groupfinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.blake.groupfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void eventsPage(View view){
+        setContentView(R.layout.events_page);
+    }
+
+    public void createPage(View view){
+        setContentView(R.layout.create);
+    }
+
+    public void chatPage(View view){
+        setContentView(R.layout.chat_room);
+    }
+
+    public void profilePage(View view){
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void loginPage(View view){
+        setContentView(R.layout.login);
+    }
+
+    public void homePage(View view){
+        setContentView(R.layout.activity_main);
+    }
 }
+
